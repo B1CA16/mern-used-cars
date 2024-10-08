8fc9c59a7660b2161f2435a664c5db7921450d71
Francisco Ferreira <121575578+B1CA16@users.noreply.github.com>
2024-10-08 21:41:45 +0100

    Car Page Highlights Section

--- frontend/src/components/CarAdPAge.jsx
+++ frontend/src/components/CarAdPAge.jsx
@@ -2,7 +2,19 @@ import React, { useContext, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { CarContext } from "../context/CarContext";
 import Slider from "react-slick";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import {
+  FaBolt,
+  FaBuilding,
+  FaCar,
+  FaCheck,
+  FaChevronDown,
+  FaChevronUp,
+  FaCogs,
+  FaGasPump,
+  FaPhone,
+  FaRoad,
+  FaUser,
+} from "react-icons/fa";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -15,12 +27,12 @@ const CarAdPage = () => {
 
   if (!car) {
     return (
-      <div className="flex flex-col items-center justify-center h-[80vh]">
+      <div className="flex flex-col items-center justify-center justify-center justify-center h-[80vh]">
         <img src="/Logo.png" alt="logo" className="w-20 h-20 mb-4" />
-        <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-200 mb-2">
+        <h2 className="text-3xl font-bold text-neutral-800 dark:text-neutral-200 mb-2">
           Car Not Found
         </h2>
-        <p className="text-gray-600 dark:text-gray-400">
+        <p className="text-neutral-600 dark:text-neutral-400">
           We couldn't find the car you're looking for.
         </p>
         <Link
@@ -43,7 +55,7 @@ const CarAdPage = () => {
       content: (
         <ul className="list-disc list-inside">
           {car.extras.audio_and_multimedia.map((feature, index) => (
-            <li key={index} className="text-gray-700 dark:text-gray-300">
+            <li key={index} className="text-neutral-800 dark:text-neutral-200">
               {feature}
             </li>
           ))}
@@ -55,7 +67,7 @@ const CarAdPage = () => {
       content: (
         <ul className="list-disc list-inside">
           {car.extras.comfort_and_other_equipment.map((feature, index) => (
-            <li key={index} className="text-gray-700 dark:text-gray-300">
+            <li key={index} className="text-neutral-800 dark:text-neutral-200">
               {feature}
             </li>
           ))}
@@ -68,7 +80,10 @@ const CarAdPage = () => {
         <ul className="list-disc list-inside">
           {car.extras.electronic_and_driving_assistance.map(
             (feature, index) => (
-              <li key={index} className="text-gray-700 dark:text-gray-300">
+              <li
+                key={index}
+                className="text-neutral-800 dark:text-neutral-200"
+              >
                 {feature}
               </li>
             )
@@ -81,7 +96,7 @@ const CarAdPage = () => {
       content: (
         <ul className="list-disc list-inside">
           {car.extras.safety.map((feature, index) => (
-            <li key={index} className="text-gray-700 dark:text-gray-300">
+            <li key={index} className="text-neutral-800 dark:text-neutral-200">
               {feature}
             </li>
           ))}
@@ -126,60 +141,104 @@ const CarAdPage = () => {
             )}
           </div>
 
+          <hr className="my-4 border-neutral-200 dark:border-neutral-800" />
+
           <div className="mt-8">
+            <h2 className="text-2xl font-semibold mb-4">Highlights</h2>
+            <div className="flex justify-around items-center mt-6">
+              <div className="flex flex-col items-center justify-center">
+                <FaRoad className="mr-2 text-3xl mb-2" />
+                <p className="text-sm text-neutral-700 dark:text-neutral-300">Mileage</p>
+                <span className="text-xl font-medium">{car.km}</span>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <FaGasPump className="mr-2 text-3xl mb-2" />
+                <p className="text-sm text-neutral-700 dark:text-neutral-300">Fuel Type</p>
+                <span className="text-xl font-medium">{car.fuel}</span>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <FaCogs className="mr-2 text-3xl mb-2" />
+                <p className="text-sm text-neutral-700 dark:text-neutral-300">Transmission</p>
+                <span className="text-xl font-medium">
+                  {car.transmission}
+                </span>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+              <svg className="mr-2 text-4xl mb-2" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path fill="currentColor" d="M11 9h6v10h-6.5l-2 -2h-2.5v-6.5l1.5 -1.5Z"/><path fill="currentColor" d="M17 13h4v-3h1v8h-1v-3h-4z"/><path d="M6 14h-4M2 11v6"/><path d="M11 9v-4M8 5h6"/></g></svg>
+                <p className="text-sm text-neutral-700 dark:text-neutral-300">Displacement</p>
+                <span className="text-xl font-medium">{car.cm3}</span>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <FaBolt className="mr-2 text-3xl mb-2" />
+                <p className="text-sm text-neutral-700 dark:text-neutral-300">Horsepower</p>
+                <span className="text-xl font-medium">{car.hp}</span>
+              </div>
+            </div>
+
+            <hr className="my-4 border-neutral-200 dark:border-neutral-800" />
+
             <h2 className="text-2xl font-semibold mb-4">Car Description</h2>
-            <p className="text-gray-700 dark:text-gray-300 text-lg">
+            <p className="text-neutral-800 dark:text-neutral-200 text-lg">
               {car.description}
             </p>
+
+            <hr className="my-4 border-neutral-200 dark:border-neutral-800" />
+
             <div className="mt-8">
               <h2 className="text-2xl font-semibold mb-4">Details</h2>
               <div className="grid grid-cols-2 gap-x-4 gap-y-2">
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Brand</strong> {car.brand}
                 </p>
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Model</strong> {car.model}
                 </p>
 
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Version</strong> {car.version}
                 </p>
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Month of Registration</strong>{" "}
                   {car.month_of_registration}
                 </p>
 
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Engine Capacity (cm³)</strong> {car.cm3}
                 </p>
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Financing Available</strong>{" "}
                   {car.financing_available ? "Yes" : "No"}
                 </p>
 
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Warranty</strong> {car.warranty}
                 </p>
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Previous Owners</strong> {car.previous_owners}
                 </p>
 
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Service Book</strong>{" "}
                   {car.service_book ? "Available" : "Not Available"}
                 </p>
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Non-Smoker</strong> {car.non_smoker ? "Yes" : "No"}
                 </p>
 
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Second Key</strong> {car.second_key ? "Yes" : "No"}
                 </p>
-                <p className="text-gray-700 dark:text-gray-300 text-lg mb-2">
+                <p className="text-neutral-800 dark:text-neutral-200 text-lg mb-2">
                   <strong>Vehicle Class</strong> {car.vehicle_class}
                 </p>
               </div>
             </div>
+
+            <hr className="my-4 border-neutral-200 dark:border-neutral-800" />
 
             <div className="mt-8">
               <h2 className="text-2xl font-semibold mb-4">Features & Extras</h2>
@@ -196,9 +255,9 @@ const CarAdPage = () => {
                       {section.title}
                     </h3>
                     {openIndex === index ? (
-                      <FaChevronUp className="text-gray-700 dark:text-gray-300" />
+                      <FaChevronUp className="text-neutral-800 dark:text-neutral-200" />
                     ) : (
-                      <FaChevronDown className="text-gray-700 dark:text-gray-300" />
+                      <FaChevronDown className="text-neutral-800 dark:text-neutral-200" />
                     )}
                   </div>
                   {openIndex === index && (
@@ -215,27 +274,50 @@ const CarAdPage = () => {
             <div>
               <h1 className="text-3xl font-bold mb-4">{car.name}</h1>
               <p className="mb-4">
-                {car.year} &#183;
-                {car.fixed_value ? "Fixed Value" : "Negotiable"} &#183;
+                {car.month_of_registration} &#183;&nbsp;
+                {car.year} &#183;&nbsp;
+                {car.fixed_value ? "Fixed Value" : "Negotiable"} &#183;&nbsp;
                 {car.financing_available ? "Financiable" : "Not Financiable"}
               </p>
               <h1 className="text-3xl font-bold mb-4">
                 {car.price} <span className="text-xl">EUR</span>
               </h1>
             </div>
-            
 
             <div className="flex flex-col mt-12 rounded-md justify-center">
               <h2 className="text-2xl font-semibold mb-2">Seller</h2>
-              {car.owner.name ? (
-                <p className="text-gray-700 dark:text-gray-300 text-lg">
-                  <strong>Owner</strong> {car.owner.name} - {car.owner.phone}
+              <div className="mb-4">
+                {car.owner.name ? (
+                  <div className="text-neutral-800 dark:text-neutral-200 text-lg">
+                    <p className="flex items-center gap-4 mb-1">
+                      <FaUser />
+                      {car.owner.name}
+                    </p>
+                    <p className="flex items-center gap-4">
+                      <FaPhone />
+                      {car.owner.phone}
+                    </p>
+                  </div>
+                ) : (
+                  <div className="text-neutral-800 dark:text-neutral-200 text-lg">
+                    <p className="flex items-center gap-4 mb-1">
+                      <FaBuilding />
+                      {car.owner.company}
+                    </p>
+                    <p className="flex items-center gap-4">
+                      <FaPhone />
+                      {car.owner.phone}
+                    </p>
+                  </div>
+                )}
+              </div>
+              {car.owner.verified ? (
+                <p className="flex items-center gap-4 text-neutral-800 dark:text-neutral-200 text-lg">
+                  <FaCheck />
+                  Verified Seller
                 </p>
               ) : (
-                <p className="text-gray-700 dark:text-gray-300 text-lg">
-                  <strong>Company</strong> {car.owner.company} -{" "}
-                  {car.owner.phone}
-                </p>
+                ""
               )}
             </div>
           </div>
