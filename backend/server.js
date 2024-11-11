54d0984dc943a3f86ff518b53a1f85e087960448
Francisco Ferreira <2221410@my.ipleiria.pt>
2024-11-11 12:35:28 +0000

    Initial server setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+//app config
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
