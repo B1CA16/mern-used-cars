fe8689f1445abab054aa4164f17dcee9a9c826f5
Francisco Ferreira <121575578+B1CA16@users.noreply.github.com>
2025-02-14 19:27:42 +0000

    Start Selling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                 <div className="flex gap-8 items-center">
                     <Link
                         to="/start-selling"
-                        className="hidden text-sm md:text-lg md:flex text-white items-center gap-2 px-5 py-[0.325rem] font-medium rounded-md border-2 border-transparent hover:scale-105 hover:border-neutral-300 hover:text-neutral-300 transition duration-300 group"
+                        className="hidden text-sm md:text-lg sm:flex text-white items-center gap-2 px-5 py-[0.325rem] font-medium rounded-md border-2 border-transparent hover:scale-105 hover:border-neutral-300 hover:text-neutral-300 transition duration-300 group"
                     >
                         <FaPlus className="transform transition-transform duration-300 group-hover:scale-110" />
                         Start selling
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,26 +7,35 @@ import FrequentlyAskedQuestions from "../components/FrequentlyAskedQuestions";
 import TipsAndMaintenance from "../components/TipsAndMaintenance";
 
 const Home = () => {
-  return (
-    <div>
-      <Header />
-      <div className="w-11/12 mx-auto">
-        <MostPopular />
-        <div className="mx-6 sm:mx-16">
-          <HomeBanner
-            image="/road.jpg"
-            title="Ready to Drive Your Dream Car?"
-            description="Explore the best deals on top-quality pre-owned cars. Drive away with confidence today."
-            buttonText="Browse Inventory"
-            buttonLink="/cars"
-          />
+    return (
+        <div>
+            <Header />
+            <div className="w-11/12 mx-auto">
+                <MostPopular />
+                <div className="mx-6 sm:mx-16">
+                    <HomeBanner
+                        image="/road.jpg"
+                        title="Ready to Drive Your Dream Car?"
+                        description="Explore the best deals on top-quality pre-owned cars. Drive away with confidence today."
+                        buttonText="Browse Inventory"
+                        buttonLink="/cars"
+                    />
+                </div>
+                <RecentlyAdded />
+                <div className="mx-6 sm:mx-16">
+                    <HomeBanner
+                        image="/road3.jpg"
+                        title="Start Selling Your Car Today!"
+                        description="Join our platform to connect with serious buyers and sell your car easily and quickly."
+                        buttonText="Start Selling"
+                        buttonLink="/start-selling"
+                    />
+                </div>
+                <FrequentlyAskedQuestions />
+                <TipsAndMaintenance />
+            </div>
         </div>
-        <RecentlyAdded />
-        <FrequentlyAskedQuestions />
-        <TipsAndMaintenance />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
