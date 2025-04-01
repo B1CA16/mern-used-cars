1772bcd4b394f11d6e23d6be62016e95903c92d6
Francisco Ferreira <2221410@my.ipleiria.pt>
2025-04-01 13:35:59 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     await mongoose
         .connect(
-            "mongodb+srv://admin:123@cluster0.o7apb.mongodb.net/SeconDrive"
+            process.env.MONGODB_URI ||
+                "mongodb+srv://admin:123@cluster0.o7apb.mongodb.net/SeconDrive"
         )
         .then(() => {
             console.log("DB Connected");
